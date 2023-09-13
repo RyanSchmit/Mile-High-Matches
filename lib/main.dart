bab18884758a76ad4e3997ef8a4240fe6d36c541
Ryan Schmitt <ryanschmitt@Ryans-Air.home>
2023-09-13 18:51:49 -0400

    Added an icon and enlarged text

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Myapp());
+  runApp(const Myapp());
 }
 
 class Myapp extends StatelessWidget {
@@ -9,34 +9,61 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red)),
       title: "Mile High Matches",
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Mile High Matches"),
       ),
-      body: Column(children: [MyTitle(), SubTitle()]),
+      body: const Column(children: [MyTitle(), SubTitle(), Airplane()]),
     );
   }
 }
 
 class MyTitle extends StatelessWidget {
+  const MyTitle({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Text("Mile High Matches");
+    return const Text(
+      "Mile High Matches",
+      style: TextStyle(fontSize: 60),
+    );
   }
 }
 
 class SubTitle extends StatelessWidget {
+  const SubTitle({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Text("Find love before you fly");
+    return const Text(
+      "Find love before you fly",
+      style: TextStyle(fontSize: 25),
+    );
+  }
+}
+
+class Airplane extends StatelessWidget {
+  const Airplane({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Icon(
+      Icons.airplane_ticket,
+      color: Colors.blue,
+      size: 100,
+    );
   }
 }
