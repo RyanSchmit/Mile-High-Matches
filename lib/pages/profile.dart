3b8c14fb4a229b665979fe49c7443e777b4a63db
Ryan Schmitt <ryan15schmitt@gmail.com>
2023-12-23 15:34:14 -0500

    Got data from firestore for the profile page

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -11,31 +12,46 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   final user = FirebaseAuth.instance.currentUser!;
 
-  // look for document that has the same id as the current users uid
+  Future getUserInfo() async {
+    var userDoc =
+        FirebaseFirestore.instance.collection("profiles").doc(user.uid);
+    return await userDoc.get().then((snapshot) => snapshot.data());
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Column(children: [
-      Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Image.asset('assets/images/profilePhoto.png')),
-      const Text("About", style: TextStyle(fontSize: 60)),
-      const Text("Full Name:",
-          style: TextStyle(fontSize: 20, color: Colors.grey)),
-      const Text("Bill Jones",
-          style: TextStyle(fontSize: 25, color: Colors.black)),
-      const Text("Bio:", style: TextStyle(fontSize: 20, color: Colors.grey)),
-      const Text("I love to fly and meet new people",
-          style: TextStyle(fontSize: 25, color: Colors.black)),
-      const Text("Email:", style: TextStyle(fontSize: 20, color: Colors.grey)),
-      Text(user.email!,
-          style: const TextStyle(fontSize: 25, color: Colors.black)),
-      MaterialButton(onPressed: () {
-        FirebaseAuth.instance.signOut();
-      }, 
-      color: Colors.lightGreen,
-      child: const Text("Sign out"),)
-    ]));
+        child: FutureBuilder(
+            future: getUserInfo(),
+            builder: (context, snapshot) {
+              return Column(
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Image.asset('assets/images/profilePhoto.png')),
+                  const Text("About", style: TextStyle(fontSize: 60)),
+                  const Text("Full Name:",
+                      style: TextStyle(fontSize: 20, color: Colors.grey)),
+                  Text(snapshot.data['name'],
+                      style: TextStyle(fontSize: 25, color: Colors.black)),
+                  const Text("Bio:",
+                      style: TextStyle(fontSize: 20, color: Colors.grey)),
+                  Text(snapshot.data['bio'],
+                      style: TextStyle(fontSize: 25, color: Colors.black)),
+                  const Text("Email:",
+                      style: TextStyle(fontSize: 20, color: Colors.grey)),
+                  Text(user.email!,
+                      style:
+                          const TextStyle(fontSize: 25, color: Colors.black)),
+                  MaterialButton(
+                    onPressed: () {
+                      FirebaseAuth.instance.signOut();
+                    },
+                    color: Colors.lightGreen,
+                    child: const Text("Sign out"),
+                  )
+                ],
+              );
+            }));
   }
 }
