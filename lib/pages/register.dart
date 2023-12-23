e4db8673126ecf3a9feec6b37eab3457147dbbf4
Ryan Schmitt <ryan15schmitt@gmail.com>
2023-12-23 00:41:16 -0500

    Changed register page to more fields

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:milehighmatch/pages/home.dart';
 
 class RegisterPage extends StatefulWidget {
   final VoidCallback showLoginPage;
@@ -11,10 +10,16 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _genderController = TextEditingController();
 
   Future signUp() async {
+    // Check if the confirm password and password are same
+    // Else show error (reference swipable method)
     await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: _emailController.text.trim(),
         password: _passwordController.text.trim());
@@ -22,8 +27,12 @@ class _RegisterPageState extends State<RegisterPage> {
 
   @override
   void dispose() {
+    _nameController.dispose();
     _emailController.dispose();
     _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    _bioController.dispose();
+    _genderController.dispose();
     super.dispose();
   }
 
@@ -32,49 +41,109 @@ class _RegisterPageState extends State<RegisterPage> {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(255, 115, 115, 1),
       body: Padding(
-        padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 6.0, bottom: 6.0),
+        padding: const EdgeInsets.only(
+            left: 16.0, right: 16.0, top: 6.0, bottom: 6.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text("Sign Up", style: TextStyle(fontSize: 24)),
             const SizedBox(height: 10),
-            const Text("Register with your info below", style: TextStyle(fontSize: 20)),
+            const Text("Register with your info below",
+                style: TextStyle(fontSize: 20)),
             const SizedBox(height: 50),
-
 
             // Input info
             Container(
               decoration: BoxDecoration(
-                color: Colors.grey[200],
-                border: Border.all(color: Colors.white)
-              ),
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.white)),
               child: TextField(
-                    cursorColor: Colors.black,
-                    controller: _emailController,
-                    decoration: InputDecoration( 
+                cursorColor: Colors.black,
+                controller: _nameController,
+                decoration: InputDecoration(
                     border: OutlineInputBorder(),
-                    labelText: "Email",
-                    labelStyle: TextStyle(color: Colors.black)
-                  ),
+                    labelText: "Name",
+                    labelStyle: TextStyle(color: Colors.black)),
               ),
             ),
-
+            const SizedBox(height: 32.0),
 
             Container(
               decoration: BoxDecoration(
-                color: Colors.grey[200],
-                border: Border.all(color: Colors.white)
-              ),
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.white)),
               child: TextField(
-                    // Make a show password boolean
-                    obscureText: true,
-                    cursorColor: Colors.black,
-                    controller: _passwordController,
-                    decoration: InputDecoration( 
+                cursorColor: Colors.black,
+                controller: _emailController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Email",
+                    labelStyle: TextStyle(color: Colors.black)),
+              ),
+            ),
+            const SizedBox(height: 32.0),
+
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.white)),
+              child: TextField(
+                // Make a show password boolean
+                obscureText: true,
+                cursorColor: Colors.black,
+                controller: _passwordController,
+                decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: "Password",
-                    labelStyle: TextStyle(color: Colors.black)
-                  ),
+                    labelStyle: TextStyle(color: Colors.black)),
+              ),
+            ),
+            const SizedBox(height: 32.0),
+
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.white)),
+              child: TextField(
+                obscureText: true,
+                cursorColor: Colors.black,
+                controller: _confirmPasswordController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Confirm Password",
+                    labelStyle: TextStyle(color: Colors.black)),
+              ),
+            ),
+            const SizedBox(height: 32.0),
+
+
+            // Change to buttons for male, female of non-binary
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.white)),
+              child: TextField(
+                cursorColor: Colors.black,
+                controller: _genderController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Gender",
+                    labelStyle: TextStyle(color: Colors.black)),
+              ),
+            ),
+            const SizedBox(height: 32.0),
+
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.white)),
+              child: TextField(
+                cursorColor: Colors.black,
+                controller: _bioController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Bio",
+                    labelStyle: TextStyle(color: Colors.black)),
               ),
             ),
             const SizedBox(height: 32.0),
@@ -89,7 +158,10 @@ class _RegisterPageState extends State<RegisterPage> {
             Column(
               children: [
                 const Text("Already a member?"),
-                GestureDetector(onTap: () => widget.showLoginPage(), child: const Text("Login", style: TextStyle(color: Colors.blue)))
+                GestureDetector(
+                    onTap: () => widget.showLoginPage(),
+                    child: const Text("Login",
+                        style: TextStyle(color: Colors.blue)))
               ],
             )
           ],
